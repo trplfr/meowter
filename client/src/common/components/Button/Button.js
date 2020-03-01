5e331dc87395476767c5bd61a9cd19d7e5dbb06d
Valentine C <trplfr@gmail.com>
2020-03-01 03:39:07 +0300

    proptypes updated

--- client/src/common/components/Button/Button.js
+++ client/src/common/components/Button/Button.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types'
 
 import { Button as Entity } from './Button.style'
 
-export const Button = ({ children }) => {
-  return <Entity>{children}</Entity>
+export const Button = ({ children, onClick }) => {
+  return <Entity onClick={onClick}>{children}</Entity>
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node,
+  onClick: PropTypes.func
 }
